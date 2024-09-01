bc05f037adc8e989c0c55cb37020ceeef2effe3f
Mamtaj Yeasmin <yeasminmamtaj33@gmail.com>
2024-09-01 23:14:27 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,8 @@
+class Array {
+public static void main(String[] args) {
+int numbers [] = {2, 4, 6,8,10};
+for (int i = numbers.length-1; i >= 0; i--) {
+System.out.print(numbers[i] + " ");
+}
+}
+}
